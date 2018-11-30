33cff135063dd2ada31e2e39ceac892e45337c17
unknown <beauhansen45@gmail.com>
2018-11-30 10:00:27 -0600

    created first file

---
+++ HistoryLists.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HistoryLists.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
